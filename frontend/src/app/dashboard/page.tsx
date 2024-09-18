ed5cf2e4d2d6ae6268c6ebbc0ce739d520209e74
luyomo <syunka.tyo@gmail.com>
2024-09-18 14:36:28 +0900

    Setup the whole page to memorize the words

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import type { ProColumns } from '@ant-design/pro-components';
 
 import Icon, {
+  DownOutlined,
   PlayCircleOutlined,
   CrownOutlined,
   InfoCircleOutlined,
@@ -23,11 +24,23 @@ import {
   ProFormText,
   ProFormDigit,
   StepsForm, } from '@ant-design/pro-components';
-import { Button, ConfigProvider, Form, message, Flex } from 'antd';
+
+import type { CountdownProps } from 'antd';
+
+import { Button, Space, ConfigProvider, Form, Select, Input, 
+         Progress, Card,
+         message, Flex, Statistic } from 'antd';
 import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { useCookies             } from 'react-cookie';
 import EikenIcon from '../../../icons/EikenIcon.tsx';
 import { createStyles } from 'antd-style';
+
+const cardStyle: React.CSSProperties = {
+  width: 780,
+  height: 480,
+  margin: 20,
+};
+
 
 const useStyle = createStyles(({ prefixCls, css }) => ({
   linearGradientButton: css`
@@ -55,12 +68,44 @@ const useStyle = createStyles(({ prefixCls, css }) => ({
   `,
 }));
 
+
+const cityData = {
+  Zhejiang: ['Hangzhou', 'Ningbo', 'Wenzhou'],
+  Jiangsu: ['Nanjing', 'Suzhou', 'Zhenjiang'],
+};
+type CityName = keyof typeof cityData;
+const provinceData: CityName[] = ['Zhejiang', 'Jiangsu'];
+
+
 export default () => {
   const [pathname    , setPathname]  = useState('/init-component');
   const [mounted     , setMounted]   = useState(false);
   const [cookies     , setCookie]    = useCookies(['AUTH_ACCESS_TOKEN'])
   const [form] = Form.useForm<{ name: string; company: string }>();
   const { styles } = useStyle();
+
+  const [cities, setCities] = useState(cityData[provinceData[0] as CityName]);
+  const [secondCity, setSecondCity] = useState(cityData[provinceData[0]][0] as CityName);
+
+  const handleProvinceChange = (value: CityName) => {
+    setCities(cityData[value]);
+    setSecondCity(cityData[value][0] as CityName);
+  };
+
+  const { Countdown } = Statistic;
+  const onSecondCityChange = (value: CityName) => {
+    setSecondCity(value);
+  };
+
+  const onFinish: CountdownProps['onFinish'] = () => {
+    console.log('finished!');
+  };
+
+  const onChange: CountdownProps['onChange'] = (val) => {
+    if (typeof val === 'number' && 4.95 * 1000 < val && val < 5 * 1000) {
+      console.log('changed!');
+    }
+  };
 
   useEffect(() => { 
     setMounted(true);
@@ -138,7 +183,41 @@ export default () => {
     </Flex>
   )
 
-  const EikenAudio2Word     = () => ("Todo: Write English Words from Audio");
+  // phase 01: Check whether it's the first time to access the data. If yes, start it to go through all the words to filter out the unremembered words.
+  // 01.01 send request to service worker. 
+  // phase 02:
+  const EikenAudio2Word     = () => (
+    <Card hoverable style={cardStyle} styles={{ body: { padding: 20, overflow: 'hidden' } }}>
+    <Flex vertical='vertical' justify='space-evenly' gap={ 50 } >
+      <Flex justify='space-evenly' align='center'>
+        <Select
+          defaultValue={provinceData[0]}
+          style={{ width: 120 }}
+          onChange={handleProvinceChange}
+          options={provinceData.map((province) => ({ label: province, value: province }))}
+        />
+        <Select
+          style={{ width: 120 }}
+          value={secondCity}
+          onChange={onSecondCityChange}
+          options={cities.map((city) => ({ label: city, value: city }))}
+        />
+        <Button type="primary">Start</Button>
+      </Flex>
+      
+      <Flex justify='space-evenly' align='center'>
+        <Progress type="circle" percent={100*18/20} format={percent => `完成率18/20` } />
+        <Progress type="circle" percent={100*10/18} format={(percent) => '成功率10/18'} />
+      </Flex>
+      <Flex justify='space-evenly' align='center'>
+        <Input placeholder="Basic usage" />
+      </Flex>
+      <Flex  justify='flex-end'>
+        <Countdown title="Seconds" value={Date.now() + 60 * 1000} format="HH:mm:ss" />
+      </Flex>
+    </Flex>
+    </Card>
+  );
   const EikenJP2Word        = () => ("Todo: Write English Words from JP");
   const EikenAudio2Sentence = () => ("Todo: Write English Sentence from Audio");
   const EikenJP2Sentence    = () => ("Todo: Write English Sentence from JP");
